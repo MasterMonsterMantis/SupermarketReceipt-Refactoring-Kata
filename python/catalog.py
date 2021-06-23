4284f589f164d333bfa92d900c7521fa28fbc8c0
MasterMonsterMantis <86256751+MasterMonsterMantis@users.noreply.github.com>
2021-06-23 23:55:41 +0430

    Update catalog.py

--- python/catalog.py
+++ python/catalog.py
@@ -2,8 +2,23 @@
 class SupermarketCatalog:
 
     def add_product(self, product, price):
-        raise Exception("cannot be called from a unit test - it accesses the database")
+        self.product = product
+        self.price = price
+        if product and price is null
+        raise TypeError("Error")
+        if product is not str
+        raise TypeError("Only string are allowed")
+        if price is not int
+        raise TypeError("Only integers are allowed")
+        
 
-    def unit_price(self, product):
-        raise Exception("cannot be called from a unit test - it accesses the database")
+    def unit_price(self, product , price):
+        self.product = product
+        self.price = price
+        if product and price is null
+        raise TypeError("Error")
+        if product is not str
+        raise TypeError("Only string are allowed")
+        if price is not int
+        raise TypeError("Only integers are allowed")
 
