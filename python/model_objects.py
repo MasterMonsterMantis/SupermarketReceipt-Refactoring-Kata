8680ab2297275b23c1fc9a89ddcf327482963c2c
MasterMonsterMantis <86256751+MasterMonsterMantis@users.noreply.github.com>
2021-06-24 12:54:09 +0430

    Update model_objects.py

--- python/model_objects.py
+++ python/model_objects.py
@@ -1,21 +1,24 @@
 from enum import Enum
 
-
+#product calss
 class Product:
     def __init__(self, name, unit):
         self.name = name
         self.unit = unit
+    
 
-
+#product No. class
 class ProductQuantity:
     def __init__(self, product, quantity):
         self.product = product
         self.quantity = quantity
 
 
-class ProductUnit(Enum):
-    EACH = 1
-    KILO = 2
+#package and pair item in a package class    
+public class productpackage:
+    def __init__(self,product)
+        self.product = product
+        product.product = product
 
 
 class SpecialOfferType(Enum):
@@ -24,6 +27,12 @@ class SpecialOfferType(Enum):
     TWO_FOR_AMOUNT = 3
     FIVE_FOR_AMOUNT = 4
 
+#new specialoffer
+class SpecialOfferNew:
+    def __init__(self,product,item)
+        self.item = item
+        self.pruduct = pruduct
+    
 class Offer:
     def __init__(self, offer_type, product, argument):
         self.offer_type = offer_type
